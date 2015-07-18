7bf1428b5f798088967181025914e446d477e753
José Luis Lorenzo Méndez <jlorenzome@telefonica.net>
2015-07-18 15:05:28 +0100

    busquedas

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Página de entrtada (home page) */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con ids
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);

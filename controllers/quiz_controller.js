5560f5c269cae5ab472c4a6367a196d748d6617f
José Luis Lorenzo Méndez <jlorenzome@telefonica.net>
2015-07-26 18:23:18 +0100

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -76,3 +76,28 @@ exports.create = function(req, res) {
     }
   }).catch(function(error){next(error)});
 };
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+    if(err) {
+      res.render('quizes/edit', {quiz: quiz, errors: err.errors});
+    } else { 
+      //guarda en DB los campos pregunta y respuesta de quiz
+      req.quiz
+      .save({fields: ["pregunta", "respuesta"]})
+      .then(function(){ res.redirect('/quizes')})
+    }
+  }).catch(function(error){next(error)});
+};

8555ac1204c866930203b380ca6aff9028ce1977
José Luis Lorenzo Méndez <jlorenzome@telefonica.net>
2015-07-28 12:48:49 +0100

    Índice temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -29,7 +29,6 @@ exports.index = function(req, res) {
         res.render('quizes/index.ejs', {quizes: quizes, errors: []});
       }
     ).catch(function(error){next(error);});
-
   }
 };
 
@@ -54,7 +53,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(  //crea objeto quiz
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
   );
   res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -71,7 +70,7 @@ exports.create = function(req, res) {
     } else { 
       //guarda en DB los campos pregunta y respuesta de quiz
       quiz
-      .save({fields: ["pregunta", "respuesta"]})
+      .save({fields: ["pregunta", "respuesta", "tema"]})
       .then(function(){ res.redirect('/quizes')})
     }
   }).catch(function(error){next(error)});
@@ -87,7 +86,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
-  
+  req.quiz.tema = req.body.quiz.tema;
   req.quiz
   .validate()
   .then(
@@ -97,7 +96,7 @@ exports.update = function(req, res) {
     } else { 
       //guarda en DB los campos pregunta y respuesta de quiz
       req.quiz
-      .save({fields: ["pregunta", "respuesta"]})
+      .save({fields: ["pregunta", "respuesta", "tema"]})
       .then(function(){ res.redirect('/quizes')})
     }
   }).catch(function(error){next(error)});

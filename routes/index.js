d9108218b458fe91955ba9278424a86a3461fced
José Luis Lorenzo Méndez <jlorenzome@telefonica.net>
2015-08-01 13:22:54 +0100

    Autenticación, sesión y autorización

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller')
 
 /* Página de entrtada (home page) */
 router.get('/', function(req, res) {
@@ -12,6 +13,11 @@ router.get('/', function(req, res) {
 // Autoload de comandos con ids
 router.param('quizId', quizController.load);  // autoload :quizId
 
+// Definicion de ruta de sesion
+router.get('/login', sessionController.new);     // formmualario login
+router.post('/login', sessionController.create); // cerar sesion
+router.get('/logout', sessionController.destroy);// destruir sesion
+
 // Definición de rutas /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -19,12 +25,13 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', function(req, res) {
   res.render('author',{errors: []});
 });
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
